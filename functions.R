67db5b9d4470f3fc7b519b87bad3fe8d89f030b8
doloresderegibus <dolidd@yahoo.com>
2018-07-18 11:02:25 -0300

    marzo

--- functions.R
+++ functions.R
@@ -1,22 +1,42 @@
-fixChlorophyllData<-function(chla) {
-  actualYear<-chla$Year[1]
-chla$Date <-ymd("1900-01-01")#aca le decimos que arme una nueva columna
-for (i in 1:nrow(chla)){
-  if (is.na(chla$Year[i])) {
+fixChlorophyllData <- function(chla){
+  actualYear <- chla$Year[1]
+  chla$Date <- ymd("1900-01-01")
+  
+  for (i in 1:nrow(chla)) {
     
-    chla$Year[i]<-actualYear
-  }else {
-    actualYear<-chla$Year[i]
+    if(is.na(chla$Year[i])) {
+      chla$Year[i] <- actualYear
+      
+    } else {
+      actualYear <- chla$Year[i]
+      
+    }
+    
+    fechaTmp<- ymd(paste(chla$Year[i], chla$Month[i], 1)) 
+    
+    if(is.na(fechaTmp)){
+      
+      if(chla$Month[i]=="Mar") {
+        
+        fechaTmp <- ymd(paste(chla$Year[i], 3, 1))
+        
+      }else if(chla$Month[i]=="Ago") {
+        
+        fechaTmp <- ymd(paste(hla$Year[i], 8, 1))
+      }  else {
+        
+        fechaTmp <- dmy(chla$Month[i])
+      }
+    } 
+    
+    chla$Date[i] <- fechaTmp
   }
-fechaTmp<-ymd(paste(chla$Year[i],chla$Month[i],1))
-if(is.na(fechaTmp)){
-  fechaTmp <-dmy(chla$Month[i])
-} 
-  chla$Date[i] <-fechaTmp
-}
-
-
-chla$IntegE1<-abs(chla$IntegE1)#para decirle que sea valor absoluto, y no negativo
-chla$IntegE2<-abs(as.numeric(chla$IntegE2))#que me considere la columna como numerico y no caracter
-return(chla)
+  
+  
+  chla$IntegE1 <- abs(chla$IntegE1)
+  chla$IntegE2 <- abs(as.numeric(chla$IntegE2))
+  
+  
+  return(chla)
+  
 }
